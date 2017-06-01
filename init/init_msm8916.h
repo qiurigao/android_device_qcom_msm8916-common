41778849d88e1216fa6d66f151499bd92dda74f0
wellqrg <qiurigao@users.noreply.github.com>
2017-06-01 13:35:06 +0800

    Update init_msm8916.h

--- init/init_msm8916.h
+++ init/init_msm8916.h
@@ -27,8 +27,8 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef __INIT_MSM8916__H__
-#define __INIT_MSM8916__H__
+#ifndef __INIT_MSM8937__H__
+#define __INIT_MSM8937__H__
 
 void init_target_properties();
 
