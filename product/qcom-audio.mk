7920ad481e819fd39699efd613ebac71868ab4a2
wellqrg <qiurigao@users.noreply.github.com>
2017-06-01 13:15:21 +0800

    Update qcom-audio.mk

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -1,7 +1,7 @@
 # Audio
 PRODUCT_PACKAGES += \
     audiod \
-    audio.primary.msm8916 \
+    audio.primary.msm8937 \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing
