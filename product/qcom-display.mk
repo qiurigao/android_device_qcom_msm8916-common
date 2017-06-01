08077ef9f536b4d85bc808deaa46432c17e190bd
wellqrg <qiurigao@users.noreply.github.com>
2017-06-01 13:17:45 +0800

    Update qcom-display.mk

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -1,10 +1,10 @@
 # Display
 PRODUCT_PACKAGES += \
-    copybit.msm8916 \
-    gralloc.msm8916 \
-    hwcomposer.msm8916 \
+    copybit.msm8937 \
+    gralloc.msm8937 \
+    hwcomposer.msm8937 \
     libtinyxml \
-    memtrack.msm8916
+    memtrack.msm8937
 
 # Permissions
 ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
@@ -20,9 +20,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.ptor.enable=true \
     debug.enable.sglscale=1
 
-ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8916)
+ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8937)
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.opengles.version=196608
+    ro.opengles.version=196610
 else
 ifeq ($(TARGET_BOARD_PLATFORM_VARIANT),msm8939)
 PRODUCT_PROPERTY_OVERRIDES += \
